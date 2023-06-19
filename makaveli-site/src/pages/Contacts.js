3244c23a44d0406d6c9bb1bb29368ede9f6ef708
Musa-Mabasa <musa14mabasa@gmail.com>
2023-06-19 20:40:42 +0200

    🐛 Fixed contact page

--- makaveli-site/src/pages/Contacts.js
+++ makaveli-site/src/pages/Contacts.js
@@ -1,48 +1,53 @@
 import React from 'react'
 import './Contacts.css'
 import * as Aiicons from 'react-icons/ai'
-import * as Mdicons from 'react-icons/md'
+import * as Faicons from 'react-icons/fa'
 import * as Bsicons from 'react-icons/bs'
 
 function Contacts() {
   return (
     <div id='wrapper'>
-    <div className='contact-header'>
-      <h1 className='header-content'>Get in touch with me for any</h1>
-      <h1 className='header-content'>further Information.</h1>
-    </div>
-      <div className='contact-box-list'>
-      <div className='contact-box'>
-        <div className='contact-icon'>
-          <Mdicons.MdEmail />
-        </div>
-        <div className='contact-text'>
-          <h3>Email:</h3>
-        </div>
-          <p className='details-text'>johndoe@gmail.com</p>
+      <div className='contact-header'>
+        <p className='header-content'>CONTACT</p>
       </div>
-      <div className='contact-box'>
-        <div className='contact-text'>
-          <h3>Social:</h3>
-        </div>
-        <div id="social-list">
-            {socialData.map((item, index) => (
-              <a href={item.link} target="_blank" rel="noopener noreferrer">
-                  {item.icon}
-              </a>
-            ))}
+        <div className='contact-box-list'>
+        <div className='contact-box'>
+          <div className='contact-text'>
+            <p>EMAIL:</p>
           </div>
-      </div>
-      <div className='contact-box'>
-        <div className='contact-icon'>
-          <Mdicons.MdPhone />
-        </div>  
-        <div className='contact-text'>
-          <h3>Phone:</h3>
-         </div>
-          <p className='details-text'>+1 234 567 890</p> 
-      </div>
-      </div>
+            <p className='details-text' style={{'text-decoration':'underline'}}>JOHNDOE@GMAIL.COM</p>
+        </div>
+        <div className='contact-box'>
+          <div className='contact-text'>
+            <p>SOCIAL:</p>
+          </div>
+          <div id="social-list">
+              {socialData.map((item, index) => (
+                <a href={item.link} target="_blank" rel="noopener noreferrer">
+                    {item.icon}
+                </a>
+              ))}
+          </div>
+        </div>
+        <div className='contact-box'>
+          <div className='contact-text'>
+            <p>PHONE:</p>
+          </div>
+            <p className='details-text'>+1 234 567 890</p> 
+        </div>
+        <div className='contact-box'>
+          <div className='contact-text'>
+            <p>STREAM:</p>
+          </div>
+          <div id="music-list">
+              {musicData.map((item, index) => (
+                <a href={item.link} target="_blank" rel="noopener noreferrer">
+                    {item.icon}
+                </a>
+              ))}
+          </div>
+        </div>
+        </div>
     </div>
   )
 }
@@ -60,6 +65,25 @@ const socialData = [
     link: 'https://www.facebook.com/',
     icon: <Bsicons.BsFacebook />
   },
+]
+
+const musicData =[
+  {
+    link: 'https://open.spotify.com/artist/3Qdtx3XbuyxpXzxBBOBffq',
+    icon: <Bsicons.BsSpotify />
+  },
+  {
+    link: 'https://music.apple.com/us/artist/raja-de-spyce/1658646151',
+    icon: <Faicons.FaItunesNote />
+  },
+  {
+    link: 'https://deezer.page.link/YtBtVVe6SJHXTZXd9',
+    icon: <Faicons.FaDeezer />
+  },
+  {
+    link: 'https://youtube.com/',
+    icon: <Faicons.FaYoutube/>
+  }
 ]
 
 export default Contacts
